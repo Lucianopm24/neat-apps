063f86ed8d98b01305e99a725779d8b9a7d000f2
Luciano <luciano24pm@gmail.com>
2026-06-07 16:10:13 -0500

    Actualizar sw.js

--- byneat/standbymode/sw.js
+++ byneat/standbymode/sw.js
@@ -1,1 +1,16 @@
-self.addEventListener('fetch', () => {});
+const CACHE = 'standby-v1';
+const ASSETS = [
+  '/byneat/standbymode/',
+  '/byneat/standbymode/index.html',
+  // agrega aquí tus CSS, JS, fuentes, etc.
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
